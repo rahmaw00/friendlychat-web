379622eaea3ecc2f125500343f18a32d8e0f8be4
rahmaw00 <ramata.will@gmail.com>
2017-11-27 23:10:17 +0000

    new version, but isn't working

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -14,10 +14,123 @@
  * limitations under the License.
  */
 
+// Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
+
+// Import the Firebase SDK for Google Cloud Functions.
+// Import and initialize the Firebase Admin SDK.
+
+const gcs = require('@google-cloud/storage')();
+const Vision = require('@google-cloud/vision');
+const vision = new Vision();
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
 // TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-
+    const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+    const admin = require('firebase-admin');
+    admin.initializeApp(functions.config().firebase);
+    
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
-
+exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  console.log('A new user signed in for the first time.');
+  const fullName = user.displayName || 'Anonymous';
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  return admin.database().ref('messages').push({
+    name: 'Firebase Bot',
+    photoUrl: '/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!` // Using back-ticks.
+  }).then(() => console.log('Welcome message written to database.'));
+});
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
-
+exports.blurOffensiveImages = functions.storage.object().onChange(event => {
+  const object = event.data;
+  // Exit if this is a deletion or a deploy event.
+  if (object.resourceState === 'not_exists') {
+    return console.log('This is a deletion event.');
+  } else if (!object.name) {
+    return console.log('This is a deploy event.');
+  }
+  const image = {
+    source: {imageUri: `gs://${object.bucket}/${object.name}`}
+  };
+  // Check the image content using the Cloud Vision API.
+  return vision.safeSearchDetection(image).then(batchAnnotateImagesResponse => {
+    const safeSearchResult = batchAnnotateImagesResponse[0].safeSearchAnnotation;
+    const Likelihood = Vision.types.Likelihood;
+    if (Likelihood[safeSearchResult.adult] >= Likelihood.LIKELY ||
+        Likelihood[safeSearchResult.violence] >= Likelihood.LIKELY) {
+      console.log('The image', object.name, 'has been detected as inappropriate.');
+      return blurImage(object.name, object.bucket);
+    } else {
+      console.log('The image', object.name,'has been detected as OK.');
+    }
+  });
+});
+function blurImage(filePath, bucketName, metadata) {
+  const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
+  const messageId = filePath.split(path.sep)[1];
+  const bucket = gcs.bucket(bucketName);
+  // Download file from bucket.
+  return bucket.file(filePath).download({destination: tempLocalFile})
+    .then(() => {
+      console.log('Image has been downloaded to', tempLocalFile);
+      // Blur the image using ImageMagick.
+      return spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
+    }).then(() => {
+      console.log('Image has been blurred');
+      // Uploading the Blurred image back into the bucket.
+      return bucket.upload(tempLocalFile, {destination: filePath});
+    }).then(() => {
+      console.log('Blurred image has been uploaded to', filePath);
+      // Deleting the local file to free up disk space.
+      fs.unlinkSync(tempLocalFile);
+      console.log('Deleted local file.');
+      // Indicate that the message has been moderated.
+      return admin.database().ref(`/messages/${messageId}`).update({moderated: true});
+    }).then(() => {
+      console.log('Marked the image as moderated in the database.');
+    });
+}
 // TODO(DEVELOPER): Write the sendNotifications Function here.
+exports.sendNotifications = functions.database.ref('/messages/{messageId}').onCreate(event => {
+  const snapshot = event.data;
+  // Notification details.
+  const text = snapshot.val().text;
+  const payload = {
+    notification: {
+      title: `${snapshot.val().name} posted ${text ? 'a message' : 'an image'}`,
+      body: text ? (text.length <= 100 ? text : text.substring(0, 97) + '...') : '',
+      icon: snapshot.val().photoUrl || '/images/profile_placeholder.png',
+      click_action: `https://${functions.config().firebase.authDomain}`
+    }
+  };
+  // Get the list of device tokens.
+  return admin.database().ref('fcmTokens').once('value').then(allTokens => {
+    if (allTokens.val()) {
+      // Listing all tokens.
+      const tokens = Object.keys(allTokens.val());
+      // Send notifications to all tokens.
+      return admin.messaging().sendToDevice(tokens, payload).then(response => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+          const error = result.error;
+          if (error) {
+            console.error('Failure sending notification to', tokens[index], error);
+            // Cleanup the tokens who are not registered anymore.
+            if (error.code === 'messaging/invalid-registration-token' ||
+                error.code === 'messaging/registration-token-not-registered') {
+              tokensToRemove.push(allTokens.ref.child(tokens[index]).remove());
+            }
+          }
+        });
+        return Promise.all(tokensToRemove);
+      });
+    }
+  });
+});
--- web-start/functions/index.js
+++ web-start/functions/index.js
@@ -16,9 +16,27 @@
 
 // Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+// Import the Firebase SDK for Google Cloud Functions.
+// Import and initialize the Firebase Admin SDK.
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(event => {
+  const user = event.data;
+  console.log('A new user signed in for the first time.');
+  const fullName = user.displayName || 'Anonymous';
+
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  return admin.database().ref('messages').push({
+    name: 'Firebase Bot',
+    photoUrl: '/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!` // Using back-ticks.
+  }).then(() => console.log('Welcome message written to database.'));
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
